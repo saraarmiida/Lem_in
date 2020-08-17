6a89129b3761dabbcd0aeafce5b025b22e1366fe
Sara Pentti <spentti@c2r4p9.hive.fi>
2020-08-17 17:24:51 +0300

    reading input

---
+++ includes/lem_in.h
@@ -0,0 +1,26 @@
+#ifndef LEM_IN_H
+# define LEM_IN_H
+
+# define BUF_SIZE	4194305
+
+typedef struct	s_room
+{
+	int			name;
+	int			x;
+	int			y;
+	int			level;
+	int			ant;
+}				t_room;
+
+typedef struct	s_lem
+{
+	t_room		**room;
+	int			ants;		/*make unsigned long*/
+	int			rooms;
+	int			links;
+	int			fd;
+	char		*input;
+}				t_lem;
+
+
+#endif

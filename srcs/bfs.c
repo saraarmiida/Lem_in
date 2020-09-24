80dd4fc9241f573fba0930945428f174694ba8fc
Sara Pentti <saraarmiida@Sara-MacBook-Pro.local>
2020-09-24 15:56:08 +0300

    starting to find paths

--- srcs/bfs.c
+++ srcs/bfs.c
@@ -8,65 +8,7 @@
 ** pointer of temp to the next one when we have iterated over the first one?
 */
 
-int		iterate_nodes(t_lem *lem, t_room *current)
-{
-	t_rlink			*current_child;
-	t_rlink			*temp;
-	t_queues		*newq;
-	static int		level;
-	static t_queues *temp_prevq;
-	static t_queues	*currentq;
-
-	if (!level)
-		level = 1;
-	current_child = current->linked_rooms;
-	if (!(newq = (t_queues*)malloc(sizeof(t_queues))))
-	{
-		ft_printf("Could not allocate queue.");
-		return (0);
-	}
-	newq->nextq = NULL;
-	newq->prevq = NULL;
-	newq->prevq = temp_prevq;
-	temp = current->linked_rooms;
-	newq->linked_rooms = temp;
-	if (temp_prevq)
-		temp_prevq->nextq = newq;
-	//ft_printf("Queue %s\n", newq->linked_rooms->room->c_name);
-	while (current_child)
-	{
-		if (current_child->room->level == 0 && lem->start != current_child->room)
-		{
-			current_child->room->level = current->level + 1;
-			ft_printf("Saved room %s | ", current_child->room->c_name);
-		}
-		if (current_child->next)
-			current_child = current_child->next;
-		else
-			current_child = NULL;
-	}
-	if (level == 1 || currentq->linked_rooms->next == NULL)
-	{
-		if (level == 1)
-			currentq = newq;
-		else if (currentq->nextq != NULL)
-			currentq = currentq->nextq;
-		else
-			return (1);
-	}
-	else
-	{
-		currentq->linked_rooms = currentq->linked_rooms->next;
-	}
-	current = currentq->linked_rooms->room;
-	level++;
-	temp_prevq = newq;
-	if (current != lem->end)
-		iterate_nodes(lem, current);
-	return(0);
-}
-
-// int		iterate_nodes(t_lem *lem, t_room *current)
+// int		level_rooms(t_lem *lem, t_room *current)
 // {
 // 	t_rlink			*current_child;
 // 	t_rlink			*temp;
@@ -127,9 +69,90 @@ int		iterate_nodes(t_lem *lem, t_room *current)
 // 	currentq = currentq->nextq;
 // 	temp_prevq = tempq;
 // 	if (current != lem->end)
-// 		iterate_nodes(lem, current);
+// 		level_rooms(lem, current);
 // 	return(0);
 // }
+
+int		level_rooms(t_lem *lem, t_room *current)
+{
+	t_rlink			*current_child;
+	t_queues		*newq;
+	static int		level;
+	static t_queues *temp_prevq;
+	static t_queues	*currentq;
+
+	if (!level)
+		level = 1;
+	current_child = current->linked_rooms;
+	if (!(newq = (t_queues*)malloc(sizeof(t_queues))))
+	{
+		ft_printf("Could not allocate queue.");
+		return (0);
+	}
+	newq->nextq = NULL;
+	newq->prevq = NULL;
+	newq->prevq = temp_prevq;
+	newq->linked_rooms = current->linked_rooms;
+	if (temp_prevq)
+		temp_prevq->nextq = newq;
+	//ft_printf("Queue %s\n", newq->linked_rooms->room->c_name);
+	while (current_child)
+	{
+		if (current_child->room->level == 0 && lem->start != current_child->room)
+		{
+			current_child->room->level = current->level + 1;
+			ft_printf("Saved room %s | ", current_child->room->c_name);
+		}
+		if (current_child->next)
+			current_child = current_child->next;
+		else
+			current_child = NULL;
+	}
+	if (level == 1 || currentq->linked_rooms->next == NULL)
+	{
+		if (level == 1)
+			currentq = newq;
+		else if (currentq->nextq != NULL)
+			currentq = currentq->nextq;
+		else
+			return (1);
+	}
+	else
+	{
+		currentq->linked_rooms = currentq->linked_rooms->next;
+	}
+	current = currentq->linked_rooms->room;
+	level++;
+	temp_prevq = newq;
+	if (current != lem->end)
+		level_rooms(lem, current);
+	return(0);
+}
+
+/* Recursive function, on each function call, we create a path from start to end,
+** we call the function again, if there still is a possibility to start a new path
+** from start room (there is an unvisited room in start->linked rooms).
+** To decide to which room to go next, we try to find an unvisited room with a higher
+** level than the current room has. If can't continue the path, we traverse back until
+** we can continue (and mark traversed rooms unvisited)
+** We save the path to a bucket of paths if it leads to end room.
+** - Do we need to delete dead ends at this point?
+** 
+*/
+
+int		find_paths(t_lem *lem)
+{
+	t_room	*current;
+	t_route	*new_route;
+
+	if (!(new_route = (t_route*)malloc(sizeof(t_route))))
+	{
+		ft_printf("Malloc failed");
+		return(0);
+	}
+
+	return (0);
+}
 
 void	bfs(t_lem *lem)
 {
@@ -140,8 +163,12 @@ void	bfs(t_lem *lem)
 	}
 	if (lem->start)
 		ft_printf("We have a start\n");
-	if (iterate_nodes(lem, lem->start) == 1)
-		ft_printf("Nodes iterated.\n");
+	if (level_rooms(lem, lem->start) == 1)
+		ft_printf("Rooms leveled.\n");
+	else
+		ft_printf("BFS did not complete.\n\nDebug info:\n");
+	if (find_paths(lem) == 1)
+		ft_printf("Paths found.\n");
 	else
 		ft_printf("BFS did not complete.\n\nDebug info:\n");
 }

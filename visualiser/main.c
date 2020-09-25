d578137d669617fde9f645c86d2a7077b4cc6c06
Ninjapiraatti <tuoppil@gmail.com>
2020-09-25 17:43:45 +0300

    Visualiser draws nodes

--- visualiser/main.c
+++ visualiser/main.c
@@ -2,50 +2,69 @@
 #include "includes/SDL.h"
 #include <stdio.h>
 
-int	drawnode(SDL_Renderer *renderer, int x, int y, int content)
+typedef struct	s_node
 {
-	SDL_Rect rect;
-	rect.x = x;
-	rect.y = y;
-	rect.w = 10;
-	rect.h = 10;
-	content++;
-	SDL_SetRenderDrawColor(renderer, 55, 255, 255, 255);
-	SDL_RenderDrawRect(renderer, &rect);
-	SDL_RenderPresent(renderer);
-	return (0);
-}
+	int			x;
+	int			y;
+	int			name;
+}				t_node;
 
-int read_lem_in(SDL_Renderer *renderer)
+int	drawnodes(SDL_Renderer *renderer, t_node *nodes)
 {
-	char 	*line;
-	int		x;
-	int		y;
-	int		content;
+	int i;
 
-	x = 0;
-	y = 0;
-	content = 0;
-	while (get_next_line(0, &line) == 1)
+	i = 0;
+	while (i < 9)
 	{
-		if (ft_strncmp(line, "Name", 3) == 0)
-		{
-			content = ft_atoi(line += 5);
-			x = ft_atoi(line += 7);
-			y = ft_atoi(line += 7);
-			x *= 10;
-			y *= 10;
-			x += 550;
-			y += 100;
-			ft_printf("Node content: %d | x: %d | y: %d\n", content, x, y);
-			drawnode(renderer, x, y, content);
-			SDL_Delay(1000);
-		}
+		SDL_Rect rect;
+		rect.x = nodes[i].x;
+		rect.y = nodes[i].y;
+		rect.w = 10;
+		rect.h = 10;
+		SDL_SetRenderDrawColor(renderer, 55, 255, 255, 255);
+		SDL_Delay(10);
+		SDL_RenderDrawRect(renderer, &rect);
+		SDL_Delay(10);
+		SDL_RenderPresent(renderer);
+		SDL_Delay(10);
+		ft_printf("Node name: %d | x: %d | y: %d\n", nodes[i].name, nodes[i].x, nodes[i].y);
+		i++;
 	}
 	return (0);
 }
 
+t_node *read_lem_in()
+{
+	char 	*line;
+	t_node 	*nodes;
+	int		x;
+	int		y;
+	int		content;
+	int		i;
+
+	x = 0;
+	y = 0;
+	content = 0;
+	i = 0;
+	if (!(nodes = (t_node*)malloc(sizeof(t_node) * 8)))
+		return (0);
+	while (get_next_line(0, &line) == 1)
+	{
+		if (ft_strncmp(line, "Name", 3) == 0)
+		{
+			nodes[i].name = ft_atoi(line += 5);
+			nodes[i].x = ft_atoi(line += 7) * 10 + 550;
+			nodes[i].y = ft_atoi(line += 7) * 10 + 100;
+			i++;
+		}
+	}
+	return (nodes);
+}
+
 int main() {
+	t_node 	*nodes;
+
+	nodes = NULL;
 	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0)
 	{
 		printf("NO SDL\n");
@@ -62,6 +81,7 @@ int main() {
 		printf("No window: %s", SDL_GetError());
 		return (0);
 	}
+	nodes = read_lem_in();
 	while (quit == 0){
 		while (SDL_PollEvent(&e)){
 			if (e.type == SDL_QUIT){
@@ -74,7 +94,8 @@ int main() {
 				quit = 1;
 			}
 		}
-		read_lem_in(renderer);
+		drawnodes(renderer, nodes);
+		//SDL_Delay(100);
 	}
 	printf("There was a window supposedly.\n");
 	SDL_DestroyWindow(win);

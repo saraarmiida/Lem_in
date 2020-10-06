28147249886b387a0fab05e2773b3799f6f3e2ef
Ninjapiraatti <tuoppil@gmail.com>
2020-10-06 14:01:37 +0300

    Changed visu params

--- visualiser/draw_text.c
+++ visualiser/draw_text.c
@@ -3,13 +3,13 @@
 int	draw_text(SDL_Renderer *renderer, t_visu *visu)
 {
 	TTF_Font	*Mono;
-	Mono = TTF_OpenFont("../assets/OverpassMono-Light.ttf", 8);
+	Mono = TTF_OpenFont("../assets/OverpassMono-Light.ttf", 10);
 	SDL_Color White = {255, 255, 255, 255}; 
-	SDL_Surface* surfaceMessage = TTF_RenderText_Solid(Mono, "Hi", White);
+	SDL_Surface* surfaceMessage = TTF_RenderText_Solid(Mono, "Hello world", White);
 	SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage); 
 	SDL_Rect Message_rect; //create a rect
-	Message_rect.x = 0;  //controls the rect's x coordinate 
-	Message_rect.y = 0; // controls the rect's y coordinte
+	Message_rect.x = 100;  //controls the rect's x coordinate 
+	Message_rect.y = 100; // controls the rect's y coordinte
 	Message_rect.w = surfaceMessage->w; // controls the width of the rect
 	Message_rect.h = surfaceMessage->h;; // controls the height of the rect
 	ft_putnbr_fd(visu->room_amount, 2);
